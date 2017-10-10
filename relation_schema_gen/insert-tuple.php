540d62f37cd136f1abd0cd9bd7b3a490e30a2872
Maxi <maximiliano.scoccimarro@gmail.com>
2017-10-10 16:44:00 -0300

    Fixes Array to String conversion
    Remove echo of Array.
    Fix spacing.

--- relation_schema_gen/insert-tuple.php
+++ relation_schema_gen/insert-tuple.php
@@ -1,44 +1,43 @@
 <?php
-    require 'connection.php';
+  require 'connection.php';
 
-    $data = json_decode($_GET['data'], true);
-    $relation = $_GET['relation'];
-    $cantidadTuplas = $_GET['cantidadTuplas'];
-    echo $data;
-    for($i = 0; $i < $cantidadTuplas; $i++) {
-       
-        $sql = "INSERT INTO " . $relation . " VALUES(";
-        
-        // use data type for query
-        foreach ($data as $item) {
-            $type = $item['type'];
-            $isAutoIncremento = $item['autoIncremento'];
-            
-            if (!preg_match('/int/i', $type)) {
-                $sql .= "'" . $item["value"];
-                if($isAutoIncremento){
-                     $sql .= "_" . $i . "'";
-                } else {
-                    $sql .= "'";
-                }
-            } else {
-                if($isAutoIncremento){
-                    $sql .= $item["value"] + $i;
-                }else {
-                    $sql .= $item["value"];
-                }
-                 
-            }
-            
-            $sql .= ', ';
-        }
-        
-        $sql = substr($sql, 0, strlen($sql)-2);
-        $sql .= ");";
-        
-        echo $sql;
-        $result = $link->query($sql);
+  $data = json_decode($_GET['data'], true);
+  $relation = $_GET['relation'];
+  $cantidadTuplas = $_GET['cantidadTuplas'];
+
+  for($i = 0; $i < $cantidadTuplas; $i++) {
+    $sql = "INSERT INTO " . $relation . " VALUES(";
+
+    // use data type for query
+    foreach ($data as $item) {
+      $type = $item['type'];
+      $isAutoIncremento = $item['autoIncremento'];
+
+      if (!preg_match('/int/i', $type)) {
+          $sql .= "'" . $item["value"];
+          if($isAutoIncremento){
+               $sql .= "_" . $i . "'";
+          } else {
+              $sql .= "'";
+          }
+      } else {
+          if($isAutoIncremento){
+              $sql .= $item["value"] + $i;
+          }else {
+              $sql .= $item["value"];
+          }
+
+      }
+
+      $sql .= ', ';
     }
-  
+
+    $sql = substr($sql, 0, strlen($sql)-2);
+    $sql .= ");";
+
+    echo $sql;
+    $result = $link->query($sql);
+  }
+
   $link->close();
 ?>
