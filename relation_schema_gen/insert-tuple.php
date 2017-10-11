ef270f2f857868db79ff6381d0d9739d4932941e
keynaka <keynaka>
2017-10-11 15:44:28 -0300

    random andando

--- relation_schema_gen/insert-tuple.php
+++ relation_schema_gen/insert-tuple.php
@@ -13,37 +13,35 @@
             $type = $item['type'];
             $isAutoIncremento = $item['autoIncremento'];
 
-            if (!preg_match('/int/i', $type)) {
-                $sql .= "'" . $item["value"];
-                if($isAutoIncremento){
-                $sql .= "_" . $i . "'";
-                } else {
-                $sql .= "'";
-                }
-            } else {
-                if($isAutoIncremento){
-                    $sql .= $item["value"] + $i;
-                }else {
-                    $sql .= $item["value"];
-                }
-
-            }
-
             $isRandom = $item['random'];
             if($isRandom) {
                 //Elegir random de $item["value"] (en $item["value"] viene una lista de pks (EN el caso que estamos viendo con key, lista de codigos IATA))
                 //Elegir codigo iata random
                 //sumarlo a la query $sql 
-                $listaPK = array_values($item["value"]);
-                $valorRandom = rand(0, count($listaPK));    
+                $listaPK = $item["value"];
+                $numRandom = rand(0, count($listaPK));    
+                $valorRandom = $listaPK[$numRandom];
                 echo $valorRandom;          
-                echo $listaPK[$valorRandom];
-                $sql .= $listaPK[$valorRandom];
+                $sql .= "'".$valorRandom."'";
+            }else {
+                if (!preg_match('/int/i', $type)) {
+                    $sql .= "'" . $item["value"];
+                    if($isAutoIncremento){
+                        $sql .= "_" . $i . "'";
+                    } else {
+                        $sql .= "'";
+                    }
+                } else {
+                    if($isAutoIncremento){
+                        $sql .= $item["value"] + $i;
+                    }else {
+                        $sql .= $item["value"];   
+                    }
+                }
             }
-
             $sql .= ', ';
         }
-
+         
         $sql = substr($sql, 0, strlen($sql)-2);
         $sql .= ");";
         echo $sql;
