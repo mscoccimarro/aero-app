049fad8bd160e5ba496840d2110d5f5ef5d31b45
Maxi <maximiliano.scoccimarro@gmail.com>
2017-10-13 15:33:31 -0300

    Adds estadoVuelo constraints.

--- relation_schema_gen/utils/vuelo_instancia_gen.php
+++ relation_schema_gen/utils/vuelo_instancia_gen.php
@@ -15,11 +15,6 @@
   }
   // var_dump($aeronaves);
 
-  $servicios = [
-    'servicioMangaLlegada',
-    'servicioMangaSalida',
-  ];
-
   $estados = [
     'inicioVentaPasajes',
     'habilitadoPorTecnico',
@@ -36,10 +31,11 @@
   $cantVuelos = 1000;
   $startDate = "07/01/2017";
   $endDate = "12/31/2017";
-  $sql = "INSERT INTO vuelo_instancia(fechaDespegue, cantidadMinimaAsientos, fechaInstanciaVuelo, fechaInicioVentaPasajes
-  servicioMangaLlegada, servicioMangaSalida, inicioVentaPasajes, habilitadoPorTecnico, reservaPuertaSalida, cargaDeCombustible, cargaDePasajeros, despachado, autorizadoDespegue, enDespegue, despegado, abordajeCerrado, matricula, codigoUnicoVuelo) VALUES";
+  $sql = "INSERT INTO vuelo_instancia(fechaDespegue, cantidadMinimaAsientos, fechaInstanciaVuelo, fechaInicioVentaPasajes, servicioMangaLlegada, servicioMangaSalida, inicioVentaPasajes, habilitadoPorTecnico, reservaPuertaSalida, cargaDeCombustible, cargaDePasajeros, despachado, autorizadoDespegue, enDespegue, despegado, abordajeCerrado, matricula, codigoUnicoVuelo) VALUES";
 
   for ($i = 0; $i < $cantVuelos; $i++) {
+    // matricula de la aeronave aleatoria y cantidad minima de asientos
+    // como el 90% de la cantidad de pasajeros de la aeronave
     $cantDePasajeros = [];
     $matIndex = rand(0, count($aeronaves)-1);
     $matricula = $aeronaves[$matIndex];
@@ -48,17 +44,42 @@
     while($row = $result->fetch_assoc()) {
       $cantDePasajeros[] = $row['cantidadDePasajeros'];
     }
-    echo $cantDePasajeros[0]."\n";
+    $cantMinAsientos = $cantDePasajeros[0] * 0.9;
+    //
 
     $vueloGenIndex = rand(0, count($vuelosGenericos)-1);
     $codUnicoVuelo = $vuelosGenericos[$vueloGenIndex];
 
+    $servicioMangaLlegada = rand(0, 1);
+    $servicioMangaSalida = rand(0, 1);
+
+    // fecha de despegue aleatoria en intervalo
+    // fecha de instancia de vuelo 30 dias antes
+    // fecha de venta de pasajes 5 dias despues de fecha de instancia de vuelo
     $min = strtotime($startDate);
     $max = strtotime($endDate);
     $time = rand($min, $max);
     $fechaDespegue = date('Y-m-d H:i:s', $time);
     $fechaInstanciaVuelo = date('Y-m-d H:i:s', $time - (30 * 24 * 60 * 60));
-    $sql .= "('{$fechaDespegue}', ,'{$fechaInstanciaVuelo}', {$codUnicoVuelo}),";
+    $fechaVentaPasajes = date('Y-m-d H:i:s', $time - (25 * 24 * 60 * 60));
+    //
+
+    // estados aleatorios de instancia de vuelo
+    $estadoActual = rand(0, count($estados)-1);
+
+    $sql .= "('{$fechaDespegue}', {$cantMinAsientos},'{$fechaInstanciaVuelo}', '{$fechaVentaPasajes}',
+    {$servicioMangaLlegada}, {$servicioMangaSalida},";
+
+    for ($j = 0; $j < count($estados); $j++) {
+      if ($j <= $estadoActual) {
+        $sql .= " 1,";
+      } else {
+        $sql .= " 0,";
+      }
+    }
+    //
+
+    $sql .= " '{$matricula}', {$codUnicoVuelo}),";
   }
 
   $sql = substr($sql, 0, strlen($sql)-1);
