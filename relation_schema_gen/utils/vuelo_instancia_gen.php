c66af925ab3baf11fcb17f3b4e67f82b5dc8b5ad
Maxi <maximiliano.scoccimarro@gmail.com>
2017-10-13 14:14:34 -0300

    Adds initial support for random values
    Random date in interval.
    Random matricula, codigoUnicoVuelo.

--- relation_schema_gen/utils/vuelo_instancia_gen.php
+++ relation_schema_gen/utils/vuelo_instancia_gen.php
@@ -33,11 +33,38 @@
     'abordajeCerrado'
   ];
 
-  $cant_vuelos = 1000;
+  $cantVuelos = 1000;
+  $startDate = "07/01/2017";
+  $endDate = "12/31/2017";
   $sql = "INSERT INTO vuelo_instancia(fechaDespegue, cantidadMinimaAsientos, fechaInstanciaVuelo, fechaInicioVentaPasajes
   servicioMangaLlegada, servicioMangaSalida, inicioVentaPasajes, habilitadoPorTecnico, reservaPuertaSalida, cargaDeCombustible, cargaDePasajeros, despachado, autorizadoDespegue, enDespegue, despegado, abordajeCerrado, matricula, codigoUnicoVuelo) VALUES";
 
+  for ($i = 0; $i < $cantVuelos; $i++) {
+    $cantDePasajeros = [];
+    $matIndex = rand(0, count($aeronaves)-1);
+    $matricula = $aeronaves[$matIndex];
+    $sql2 = "SELECT cantidadDePasajeros FROM aeronave WHERE matricula=". $matricula;
+    $result = $link->query($sql2);
+    while($row = $result->fetch_assoc()) {
+      $cantDePasajeros[] = $row['cantidadDePasajeros'];
+    }
+    echo $cantDePasajeros[0]."\n";
+
+    $vueloGenIndex = rand(0, count($vuelosGenericos)-1);
+    $codUnicoVuelo = $vuelosGenericos[$vueloGenIndex];
+
+    $min = strtotime($startDate);
+    $max = strtotime($endDate);
+    $time = rand($min, $max);
+    $fechaDespegue = date('Y-m-d H:i:s', $time);
+    $fechaInstanciaVuelo = date('Y-m-d H:i:s', $time - (30 * 24 * 60 * 60));
+    $sql .= "('{$fechaDespegue}', ,'{$fechaInstanciaVuelo}', {$codUnicoVuelo}),";
+  }
+
+  $sql = substr($sql, 0, strlen($sql)-1);
+  $sql .= ";";
   echo $sql;
+  // $link->query($sql);
 
   $link->close();
 ?>
